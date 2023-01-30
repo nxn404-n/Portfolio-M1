6c8c41588fbfa3ed5f4d9cdb89276b1cc65d742a
nxn_404 <nafeurrahmanadil@gmail.com>
2023-01-31 05:59:02 +0600

    Fixed according linters

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menuItem');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
